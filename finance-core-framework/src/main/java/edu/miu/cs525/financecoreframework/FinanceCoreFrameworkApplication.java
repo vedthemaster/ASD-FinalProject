14e65a826f5079e7a7570c4d60bc5d9fcb3af92e
Ved Patel <vedpatel3395@gmail.com>
2024-05-20 12:10:13 -0500

    Initial commit & added boiler code for framework

---
+++ finance-core-framework/src/main/java/edu/miu/cs525/financecoreframework/FinanceCoreFrameworkApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs525.financecoreframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceCoreFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceCoreFrameworkApplication.class, args);
+	}
+
+}
---
+++ finance-core-framework/src/test/java/edu/miu/cs525/financecoreframework/FinanceCoreFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs525.financecoreframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceCoreFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
