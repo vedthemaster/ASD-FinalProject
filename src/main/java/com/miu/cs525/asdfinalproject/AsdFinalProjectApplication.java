321dea898760bf87dea2485bf0a6d20a22ce7c30
Dinh Trung <trantrung140219@gmail.com>
2024-05-20 20:07:28 -0500

    init project and add ui package to it

---
+++ src/main/java/com/miu/cs525/asdfinalproject/AsdFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.miu.cs525.asdfinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsdFinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsdFinalProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/miu/cs525/asdfinalproject/AsdFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miu.cs525.asdfinalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsdFinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
