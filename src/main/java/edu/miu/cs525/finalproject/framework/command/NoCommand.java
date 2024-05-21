a658f74b41983e3495b741df68fde00d42f83947
Ved Patel <vedpatel3395@gmail.com>
2024-05-20 21:39:15 -0500

    Added command package

---
+++ src/main/java/edu/miu/cs525/finalproject/framework/command/NoCommand.java
@@ -0,0 +1,8 @@
+package edu.miu.cs525.finalproject.framework.command;
+
+public class NoCommand implements Command{
+    @Override
+    public void execute() throws Exception {
+        // do nothing
+    }
+}
