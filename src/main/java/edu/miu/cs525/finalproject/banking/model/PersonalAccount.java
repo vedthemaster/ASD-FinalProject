09a486bb0e15fff9f52845245800d87ad9e2d08b
Dinh Trung <trantrung140219@gmail.com>
2024-05-22 10:26:02 -0500

    add EmailSender to send email to account

--- src/main/java/edu/miu/cs525/finalproject/banking/model/PersonalAccount.java
+++ src/main/java/edu/miu/cs525/finalproject/banking/model/PersonalAccount.java
@@ -1,6 +1,7 @@
 package edu.miu.cs525.finalproject.banking.model;
 
 import edu.miu.cs525.finalproject.framework.model.Party;
+import edu.miu.cs525.finalproject.framework.model.Transaction;
 import edu.miu.cs525.finalproject.framework.strategy.InterestStrategy;
 
 public class PersonalAccount extends Account {
