56b84a0cb06a514d90607d7a633f06e558efed2b
Arif <arahman.isrt@gmail.com>
2024-05-22 08:12:00 -0500

    add report service

---
+++ src/main/java/edu/miu/cs525/finalproject/banking/service/ReportService.java
@@ -0,0 +1,10 @@
+package edu.miu.cs525.finalproject.banking.service;
+
+//import edu.miu.cs525.finalproject.framework.service.ReportService;
+
+public class ReportService implements edu.miu.cs525.finalproject.framework.service.ReportService {
+    @Override
+    public String generateReport() {
+        return "";
+    }
+}
---
+++ src/main/java/edu/miu/cs525/finalproject/banking/service/implement/ReportServiceImpl.java
@@ -0,0 +1,61 @@
+package edu.miu.cs525.finalproject.banking.service.implement;
+
+import edu.miu.cs525.finalproject.banking.factory.ServiceFactory;
+import edu.miu.cs525.finalproject.banking.model.Account;
+import edu.miu.cs525.finalproject.banking.repository.AccountRepository;
+import edu.miu.cs525.finalproject.banking.repository.implement.AccountRepositoryImpl;
+import edu.miu.cs525.finalproject.banking.service.AccountService;
+import edu.miu.cs525.finalproject.banking.service.ReportService;
+import edu.miu.cs525.finalproject.framework.model.Transaction;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.*;
+
+public class ReportServiceImpl extends ReportService {
+
+    private AccountService accountService;
+
+    public ReportServiceImpl(){
+        this.accountService = ServiceFactory.getAccountService();
+    }
+
+    public String generateReport() {
+
+        StringBuilder report = new StringBuilder();
+
+        accountService.getAllAccounts().forEach(
+                account -> {
+            report.append(generateReport(account));
+            report.append("\n----------------------------------------------\n\n");
+        });
+
+        return report.toString();
+    }
+
+    public String generateReport(Account account) {
+        double totalCharges = 0;
+        double totalCredits = 0;
+        Month thisMonth = LocalDate.now().getMonth();
+
+        List<Transaction> lastMonthTransactions = account
+                .getTransactions()
+                .stream()
+                .filter(entry -> entry.getDate().getMonth().equals(thisMonth))
+                .toList();
+
+        for (Transaction t : lastMonthTransactions) {
+            if (t.getAmount() < 0)
+                totalCharges += t.getAmount();
+            else
+                totalCredits += t.getAmount();
+        }
+
+        double previousBalance = account.getBalance() - totalCredits + totalCharges;
+
+        return account.getCustomer().getName() + ": " +
+                account.getAccountType()  + "-" + account.getAccountNumber() + "\n"  +
+                "Last Month's account balance : '" + previousBalance + "'\n" +
+                "Current Month's account balance: '" + account.getBalance() + "'" ;
+    }
+}
