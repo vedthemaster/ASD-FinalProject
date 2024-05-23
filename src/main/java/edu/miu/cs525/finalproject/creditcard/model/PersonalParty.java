b503fc7a0081c4dd799d07ef7a266e620b78d7e5
Ko Phone <phonethantzaw@gmail.com>
2024-05-23 11:03:05 -0500

    Rename Account to Party

--- src/main/java/edu/miu/cs525/finalproject/creditcard/model/PersonalParty.java
+++ src/main/java/edu/miu/cs525/finalproject/creditcard/model/PersonalParty.java
@@ -4,12 +4,11 @@ import edu.miu.cs525.finalproject.framework.model.Account;
 import edu.miu.cs525.finalproject.framework.model.Address;
 import edu.miu.cs525.finalproject.framework.model.Party;
 import edu.miu.cs525.finalproject.framework.model.Transaction;
-import edu.miu.cs525.finalproject.framework.strategy.InterestStrategy;
 
 
-public class PersonalAccount extends Party {
+public class PersonalParty extends Party {
 
-    public PersonalAccount(String name, String email, Address address) {
+    public PersonalParty(String name, String email, Address address) {
         super(name, email, address);
     }
 
